5f057b5fb9af97d962b4015987e1a2ac80301a1d
Shane-BR <beastiethepineapple@gmail.com>
2023-12-28 14:51:51 +1300

    build.bat changed to echo success and failure.

--- build.bat
+++ build.bat
@@ -1,3 +1,9 @@
+@echo off
 @echo Building application
 clang src/*.c lib/glad/src/glad.c -o bin/natsim.exe  -I includes/nat-sim -I includes -I includes/GLFW -Llib/GLFW/lib-vc2022 -lglfw3dll -lgdi32 -lopengl32
-
+IF %ERRORLEVEL% NEQ 0 (
+    echo Build Failed
+    exit /b %errorlevel%
+) ELSE (
+    echo Build Successful
+)
