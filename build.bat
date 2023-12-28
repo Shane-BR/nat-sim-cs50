b78c0e3f9bc4f3e5fa3bfc01140f5a1900f94c40
Shane-BR <beastiethepineapple@gmail.com>
2023-12-28 14:52:25 +1300

    Capital A oops

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-@echo Building application
+@echo Building Application
 clang src/*.c lib/glad/src/glad.c -o bin/natsim.exe  -I includes/nat-sim -I includes -I includes/GLFW -Llib/GLFW/lib-vc2022 -lglfw3dll -lgdi32 -lopengl32
 IF %ERRORLEVEL% NEQ 0 (
     echo Build Failed
